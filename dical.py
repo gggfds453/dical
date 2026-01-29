546e826b0f703554a9dff6f3a0485514f5065141
gggfds453 <155727447+gggfds453@users.noreply.github.com>
2026-01-29 23:39:50 +0800

    Update dical.py

--- dical.py
+++ dical.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("dilCal")
-st.write("輸入原始濃度 $a \\times 10^b$ 體積 $c$，和目標濃度 $d \\times 10^e$ 體積 $f")
+st.write("輸入原始濃度 $a \\times 10^b$ 體積 $c$，和目標濃度 $d \\times 10^e$ 體積 $f$")
 
 col1, col2 = st.columns(2)
 
@@ -16,6 +16,7 @@ with col2:
     d = st.number_input("輸入係數 d", value=0.0, step=1)
     e = st.number_input("輸入冪次 e", value=0, step=1)
     f = st.number_input("輸入體積 f (µL)", value=0.0, step=1)
+
 
 
 
