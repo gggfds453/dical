6195ed016e59ac876b8b3f345541ca75dbf11736
gggfds453 <155727447+gggfds453@users.noreply.github.com>
2026-01-29 23:42:40 +0800

    Update dical.py

--- dical.py
+++ dical.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title("dilCal")
 st.write("輸入原始濃度 $a \\times 10^b$ 體積 $c$，和目標濃度 $d \\times 10^e$ 體積 $f$")
+st.write("自行注意因次單位一致 然後不要輸一些奇怪數字搞它")
 
 col1, col2 = st.columns(2)
 
@@ -9,13 +10,14 @@ with col1:
     st.subheader("原始資料")
     a = st.number_input("輸入係數 a", value=0.0, step=None)
     b = st.number_input("輸入冪次 b", value=0, step=None)
-    c = st.number_input("輸入體積 c (µL)", value=0.0, step=None)
+    c = st.number_input("輸入體積 c", value=0.0, step=None)
 
 with col2:
     st.subheader("最終目標")
     d = st.number_input("輸入係數 d", value=0.0, step=None)
     e = st.number_input("輸入冪次 e", value=0, step=None)
-    f = st.number_input("輸入體積 f (µL)", value=0.0, step=None)
+    f = st.number_input("輸入體積 f", value=0.0, step=None)
+
 
 
 
