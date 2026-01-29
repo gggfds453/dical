ba76e6589d3a42c52bcf8345b5b35a3d5d4f5825
gggfds453 <155727447+gggfds453@users.noreply.github.com>
2026-01-29 23:34:16 +0800

    Update dical.py

--- dical.py
+++ dical.py
@@ -7,15 +7,16 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.subheader("原始資料")
-    a = st.number_input("輸入係數 a", value=1.0, step=0.1)
-    b = st.number_input("輸入冪次 b", value=9, step=1)
-    c = st.number_input("輸入體積 c (µL)", value=100.0, step=1.0)
+    a = st.number_input("輸入係數 a", value=0.0, step=None)
+    b = st.number_input("輸入冪次 b", value=0, step=None)
+    c = st.number_input("輸入體積 c (µL)", value=0.0, step=None)
 
 with col2:
     st.subheader("最終目標")
-    d = st.number_input("輸入係數 d", value=1.0, step=0.1)
-    e = st.number_input("輸入冪次 e", value=3, step=1)
-    f = st.number_input("輸入體積 f (µL)", value=1000.0, step=1.0)
+    d = st.number_input("輸入係數 d", value=0.0, step=None)
+    e = st.number_input("輸入冪次 e", value=0, step=None)
+    f = st.number_input("輸入體積 f (µL)", value=0.0, step=None)
+
 
 
 
