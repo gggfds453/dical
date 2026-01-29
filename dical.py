84dbedb4e58b2a540ea7eadb4eb137806793df6b
gggfds453 <155727447+gggfds453@users.noreply.github.com>
2026-01-29 23:30:34 +0800

    Update dical.py

--- dical.py
+++ dical.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 st.title("dilCal")
-st.write("輸入原始濃度 $a \\times 10^b$ 體積 $c$，以及最終濃度 $d \\times 10^e$ 體積 $f$ 自行注意因次")
+st.write("輸入原始濃度 $a \\times 10^b$ 體積 $c$，和目標濃度 $d \\times 10^e$ 體積 $f$     自行注意因次")
+
 
 
 
